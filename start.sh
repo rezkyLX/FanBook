8cdabc20abce5b22cfdbdbb9ae749f2c98251fd4
rezkyLX <lunarknight000@gmail.com>
2024-05-25 10:07:38 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 set -eu
 
-export PYTHONNUNBUFFERED=true
+export PYTHONUNBUFFERED=true
 
 VIRTUALENV=.data/venv
 
-if [ ! -d $VIRTUALENV]; then
-    python3 -m venv $VIRTUALENV
+if [ ! -d $VIRTUALENV ]; then
+  python3 -m venv $VIRTUALENV
 fi
 
 if [ ! -f $VIRTUALENV/bin/pip ]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
