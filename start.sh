ef7626aeba13942463cb1b1e57cbba4f2ca57ffd
rezkyLX <lunarknight000@gmail.com>
2024-05-25 10:03:34 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ if [ ! -d $VIRTUALENV]; then
     python3 -m venv $VIRTUALENV
 fi
 
-if [ ! -d $VIRTUALENV/bin/pip ]; then
+if [ ! -f $VIRTUALENV/bin/pip ]; then
     curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV
 fi
 
